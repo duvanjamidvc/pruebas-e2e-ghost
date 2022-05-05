f70065bf4b28ffd06660e66e6f41670a8e60c9d2
plinio <p-grijalbar@uniandes.edu.co>
2022-05-04 19:13:13 -0500

    Como usuario inicio sesion, creo un post y lo público

--- kraken-e2e-ghost/features/my_first.feature
+++ kraken-e2e-ghost/features/my_first.feature
@@ -1,14 +1,20 @@
 Feature: My feature
 
 @user1 @web
-Scenario: Como usuario 2 inicio sesion y mandó un mensaje al usuario 1
+Scenario: Como usuario inicio sesion, creo un post y lo público
   Given I navigate to page "<URL_GHOST>"
-  And I wait for 3 seconds
+  And I wait for 2 seconds
   When I enter email "<EMAIL>"
   And I wait for 1 seconds
   And I enter password "<PASSWORD>"
   And I wait for 1 seconds
   And I click login
-  And I wait for 70 seconds
+  And I wait for 3 seconds
   And I click posts
-  And I wait for 10 seconds
+  And I wait for 2 seconds
+  And I click new post
+  And I wait for 2 seconds
+  And I write title a post
+  And I wait for 2 seconds
+  And I publish a post and verify
+  And I wait for 5 seconds
--- kraken-e2e-ghost/features/web/step_definitions/step.js
+++ kraken-e2e-ghost/features/web/step_definitions/step.js
@@ -1,4 +1,4 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const {Given, When, Then} = require('@cucumber/cucumber');
 
 
 When('I enter email {kraken-string}', async function (email) {
@@ -18,7 +18,7 @@ When('I enter password {kraken-string}', async function (password) {
 });
 
 
-When('I click login', async function() {
+When('I click login', async function () {
 
     let element = await this.driver.$('#ember11');
 
@@ -26,10 +26,54 @@ When('I click login', async function() {
 
 });
 
-When('I click posts', async function() {
+When('I click posts', async function () {
 
-    let element = await this.driver.$('#ember340');
+    let element = await this.driver.$('a[href="#/posts/"]');
 
     return await element.click();
 
 });
+
+When('I click new post', async function () {
+
+    let element = await this.driver.$('.view-actions > a[href="#/editor/post/"]');
+
+    return await element.click();
+
+});
+
+
+When('I write title a post', async function () {
+
+    let element = await this.driver.$('textarea[autofocus]');
+
+    return await element.setValue('Nuevo post');
+
+});
+
+When('I publish a post', async function () {
+
+    let elementPrev = await this.driver.$('.settings-menu-toggle');
+    await elementPrev.click();
+    let element = await this.driver.$('.gh-publishmenu .gh-publishmenu-trigger');
+    await element.click();
+    let btnPublish = await this.driver.$('.gh-publishmenu-button');
+    return await btnPublish.click();
+
+});
+
+When('I publish a post and verify', async function () {
+
+    let elementPrev = await this.driver.$('.settings-menu-toggle');
+    await elementPrev.click();
+    let element = await this.driver.$('.gh-publishmenu .gh-publishmenu-trigger');
+    await element.click();
+    let btnPublish = await this.driver.$('.gh-publishmenu-button');
+    await btnPublish.click();
+    let elementUrl = await this.driver.$('#url');
+    await new Promise(function (r) { return setTimeout(r, 1000 * 3); })
+    let url = await elementUrl.getValue();
+    return this.driver.url(url);
+});
+
+
