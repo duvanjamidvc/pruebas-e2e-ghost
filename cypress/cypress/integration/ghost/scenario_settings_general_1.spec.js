c2454a4c53bedbb3c8b4f428c99f9ba9bbe902c1
Viviana Ojeda <m.ojedan@uniandes.edu.co>
2022-05-06 15:27:46 -0500

    cambios y resolución escenarios

--- cypress/cypress/integration/ghost/scenario_settings_general_1.spec.js
+++ cypress/cypress/integration/ghost/scenario_settings_general_1.spec.js
@@ -2,69 +2,69 @@
 
 let usuarios;
 describe("General Settings", () => {
-  before(() => {
-    cy.fixture("users").then((users) => {
-      usuarios = users;
-    });
-  });
+	before(() => {
+		cy.fixture("users").then((users) => {
+			usuarios = users;
+		});
+	});
 
-  beforeEach(() => {
-    cy.login(usuarios.admins[0].username, usuarios.admins[0].password);
-  });
+	beforeEach(() => {
+		cy.login(usuarios.admins[0].username, usuarios.admins[0].password);
+	});
 
-  it("set private site and validate incorrect password ", () => {
-    const url = Cypress.config("baseUrl");
-    const newPassword = cy.faker.internet.password();
-    const incorrectPassword = newPassword + cy.faker.datatype.number();
-    cy.goToGeneralSettings();
-    //valida si el sitioes privado y si no lo es asigna privado
-    cy.get("body").then(($body) => {
-      if ($body.find('[name="general[password]"]').length <= 0) {
-        cy.get(
-          "body > div.gh-app > div > main > section > div:nth-child(4) > section > div > div.gh-expandable-header > div.for-switch > label > span"
-        ).click();
-      }
-      //asigna contraseña
-      cy.get('[name="general[password]"]').type(newPassword);
-    });
-    //guarda
-    cy.get(
-      ".gh-canvas-header > .gh-canvas-header-content > .view-actions "
-    ).click();
-    // esperamos que el guardado sea existoso
-   
-    cy.wait(5000)
-    //visita el sitio
-    cy.visit(url);
-    //digita contraseña incorrecta
-    cy.get('[name="password"]').type(incorrectPassword);
-    cy.get('button[type="submit"]').click();
-    //verifica que la contraseña sea incorrecta
-    cy.get(
-      ".gh-flow-content-wrap > .gh-flow-content > .gh-signin > .form-group "
-    ).should("contain", "Incorrect password.");
-  });
+	it("set private site and validate incorrect password ", () => {
+		const newPassword = cy.faker.internet.password();
+		cy.goToGeneralSettings();
+		//valida si el sitioes privado y si no lo es asigna privado
+		cy.get("body").then(($body) => {
+			if ($body.find('[name="general[password]"]').length <= 0) {
+				cy.get(
+					"body > div.gh-app > div > main > section > div:nth-child(4) > section > div > div.gh-expandable-header > div.for-switch > label > span"
+				).click();
+			}
+			//asigna contraseña
+			cy.get('[name="general[password]"]').invoke("val", "").type(newPassword);
+		});
+		//guarda
+		cy.get(
+			".gh-canvas-header > .gh-canvas-header-content > .view-actions "
+		).click();
+		// esperamos que el guardado sea existoso
 
-  it("Set public site", () => {
-    cy.goToGeneralSettings();
-    //vuelve a poner el sitio como publico
-    cy.get(
-      "body > div.gh-app > div > main > section > div:nth-child(4) > section > div > div.gh-expandable-header > div.for-switch > label > span"
-    ).click();
-    //guarda
-    cy.get(
-      ".gh-canvas-header > .gh-canvas-header-content > .view-actions "
-    ).click();
-    cy.wait(500);
-  });
+		cy.wait(5000);
+		//va a dashboard
+		cy.get('a[href="#/dashboard/"]').parent().first().click();
+		//va de nuevo a settings
+		cy.goToGeneralSettings();
+		//valida que la contraseña sea la guardada
 
-  after(() => {
-    cy.closeDashBoardSession();
-  });
+		cy.get('input[name="general[password]"]').click({ force: true });
+		cy.get('input[name="general[password]"]').should(
+			"have.value",
+			newPassword
+		);
+	});
+
+	it("Set public site", () => {
+		cy.goToGeneralSettings();
+		//vuelve a poner el sitio como publico
+		cy.get(
+			"body > div.gh-app > div > main > section > div:nth-child(4) > section > div > div.gh-expandable-header > div.for-switch > label > span"
+		).click();
+		//guarda
+		cy.get(
+			".gh-canvas-header > .gh-canvas-header-content > .view-actions "
+		).click();
+		cy.wait(500);
+	});
+
+	after(() => {
+		cy.closeDashBoardSession();
+	});
 });
 
 Cypress.on("uncaught:exception", (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false;
+	// returning false here prevents Cypress from
+	// failing the test
+	return false;
 });
