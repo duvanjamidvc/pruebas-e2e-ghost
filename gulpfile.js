a4f61851407d3e290f6ae38eef484612e4164fbd
plinio <p-grijalbar@uniandes.edu.co>
2022-05-13 00:29:09 -0500

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,41 @@
 const gulp = require('gulp')
 const shell = require('gulp-shell')
 
-gulp.task('example', async () => {
-    gulp.src('/kraken-e2e-ghost-v3.42/')
-        .pipe(shell.task('npm start'));
+
+
+gulp.task('kraken-e2e-ghost-v3.42', () => {
+    process.chdir(__dirname+'/kraken-e2e-ghost-v3.42');
+    return gulp.src(".")
+        .pipe(shell('npm install'))
+        .pipe(shell('npm start'));
 })
+
+gulp.task('kraken-e2e-ghost-v4.41.3', () => {
+    process.chdir(__dirname+'/kraken-e2e-ghost-v4.41.3');
+    return gulp.src(".")
+        .pipe(shell('npm install'))
+        .pipe(shell('npm start'));
+})
+
+gulp.task('cypress-v3', () => {
+    process.chdir(__dirname+'/cypress-v3');
+    return gulp.src(".")
+        .pipe(shell('npm install'))
+        .pipe(shell('npm start'));
+})
+
+gulp.task('cypress-v4.41.3', () => {
+    process.chdir(__dirname+'/cypress-v4.41.3');
+    return gulp.src(".")
+        .pipe(shell('npm install'))
+        .pipe(shell('npm start'));
+})
+
+gulp.task('resemblejs', () => {
+    process.chdir(__dirname+'/resemblejs');
+    return gulp.src(".")
+        .pipe(shell('npm install'))
+        .pipe(shell('npm start'));
+})
+
+gulp.task('all-run', gulp.series('kraken-e2e-ghost-v3.42', 'kraken-e2e-ghost-v4.41.3', 'cypress-v4.41.3', 'cypress-v3', 'resemblejs'));
