b345484dfb144b19f720235dbe28f6bc9c59bdf3
plinio <p-grijalbar@uniandes.edu.co>
2022-05-20 19:02:49 -0500

    test profile scenarios v1

---
+++ cypress-gen-datos/cypress/integration/pageObject/profilePage.js
@@ -0,0 +1,57 @@
+class ProfilePage {
+	navegate() {
+		const url = Cypress.config('baseUrlDashBoard');
+		cy.visit(url);
+		cy.goUserProfile();
+	}
+
+	inputUser() {
+		return cy.get('#user-name');
+	}
+	inputUserSlug() {
+		return cy.get('#user-slug');
+	}
+
+	inputEmail() {
+		return cy.get('#user-email');
+	}
+
+	inputLocation() {
+		return cy.get('#user-location');
+	}
+
+	inputWebSite() {
+		return cy.get('#user-website');
+	}
+
+	inputFacebook() {
+		return cy.get('#user-facebook');
+	}
+
+	inputTwitter() {
+		return cy.get('#user-twitter');
+	}
+	
+	inputBio() {
+		return cy.get('#user-bio');
+	}
+
+	inputPassOld() {
+		return cy.get('#user-password-old');
+	}
+
+	inputPassNew() {
+		return cy.get('#user-password-new');
+	}
+
+	inputPassNewVerification() {
+		return cy.get('#user-new-password-verification');
+	}
+	
+	errorMessage() {
+		return cy.get('.error .response');
+	}
+
+}
+
+export default ProfilePage;
